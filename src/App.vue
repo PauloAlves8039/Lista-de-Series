dc32e9d482bd0ddc8395036b25b574b89554330f
PauloAlves8039 <pj38alves@gmail.com>
2020-05-05 01:02:40 -0300

    Implementando template no arquivo App

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,25 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <div class="container">
+    <nav class="navbar navbar-dark bg-dark justify-content-between flex-nowrap flex-row">
+      <div class="container">
+        <a class="navbar-brand float-left">Catálogo de Séries</a>
+        <ul class="nav navbar-nav flex-row float-right">
+          <li class="nav-item">
+            <router-link class="nav-link pr-3" to="/">Adicionar Série</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link pr-3" to="/listar">Lista de Séries</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/About">Sobre</router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <div class="container mt-5">
+      <router-view></router-view>
     </div>
-    <router-view/>
   </div>
 </template>
 
