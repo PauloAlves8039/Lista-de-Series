46bed9578f56602045ed2af06d30aab7b2306c2a
PauloAlves8039 <pj38alves@gmail.com>
2020-05-05 00:54:58 -0300

    Adicionando configuração de rotas

--- src/router/index.js
+++ src/router/index.js
@@ -7,20 +7,29 @@
 
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'adicionar',
+    component: () => import('../components/AdicionarSeries.vue')
+  },
+  {
+    path: '/listar',
+    name: 'listar',
+    component: () => import('../components/ListarSeries.vue')
+  },
+  {
+    path: '/editar/:id',
+    name: 'editar',
+    component: () => import('../components/EditarSeries.vue')
   },
   {
     path: '/about',
     name: 'About',
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import('../views/About.vue')
   }
 ]
 
